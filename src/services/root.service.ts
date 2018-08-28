76610161c3fdc2c92904a67f610c1e282663d7bc
dzzzzzy <insistyao_0522@163.com>
2018-08-28 12:41:47 +0800

    chore(*): upgrade packages version

--- src/services/root.service.ts
+++ src/services/root.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class RootService {
     async sayHello(name: string) {
-        return `Hello ${name}!`;
+        return { msg: `Hello ${name}!` };
     }
 }
